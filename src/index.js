ab3f32f6b7771ca871d43d911bb1245f9d57885e
Anti6ot <kradenihs@gmail.com>
2023-05-25 00:06:09 +0500

    taskReducer, createStore

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function createStore(initialState) {
+function taskReducer(state, action) {
+  switch (action.type) {
+    case "task/completed":
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      return newArray;
+
+    default:
+      break;
+  }
+}
+const store = createStore(taskReducer, [
+  {
+    id: 1,
+    description: "Task 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    description: "Task 2",
+    completed: false,
+  },
+]);
+
+function createStore(reducer, initialState) {
   let state = initialState;
 
   function getState() {
     return state;
   }
   function dispatch(action) {
-    console.log(action);
-    if (action.type === "task/completed") {
-      const newArray = [...state];
-      const elementIndex = newArray.findIndex(
-        (el) => el.id === action.payload.id
-      );
-      newArray[elementIndex].completed = true;
-      state = newArray;
-      console.log(state);
-    }
+    state = reducer(state, action);
   }
   return {
     getState,
@@ -27,27 +44,26 @@ function createStore(initialState) {
   };
 }
 
-const store = createStore([
-  {
-    id: 1,
-    description: "Task 1",
-    completed: false,
-  },
-]);
-
 const App = (params) => {
-  console.log(store.getState());
+  const state = store.getState();
 
+  const completeTask = (taskId) => {
+    store.dispatch({ type: "task/completed", payload: { id: taskId } });
+
+    console.log(store.getState());
+  };
   return (
     <>
-      <h1>App</h1>{" "}
-      <button
-        onClick={() =>
-          store.dispatch({ type: "task/completed", payload: { id: 1 } })
-        }
-      >
-        completed
-      </button>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.description}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>completed</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
